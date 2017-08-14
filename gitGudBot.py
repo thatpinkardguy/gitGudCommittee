86f8b61e47e9eb462be5ee72a4b189df773a3adf
Ryan Pinkard <thatpinkardguy@gmail.com>
2017-08-14 03:52:57 +0000

    updated to use configparser

--- gitGudBot.py
+++ gitGudBot.py
@@ -2,12 +2,36 @@
 import praw
 import json
 from pprint import pprint
+import configparser as ConfigParser
+#from pydash import _
 
 def authenticate(config):
 
 	#authenticate with Reddit
+
+	client_id = config.get("Credentials", "client_secret")
+	client_secret = config.get("Credentials", "client_secret")
+	username = config.get("Credentials", "username")
+	password = config.get("Credentials", "password")
+	user_agent = config.get("Credentials", "user_agent")
+
+
 	print("Attempting to authenticate....")
-	reddit = praw.Reddit(client_id=config["client_id"], client_secret=config["client_secret"], username=config["username"],password=config["password"],user_agent=config["user_agent"])
+	#reddit = praw.Reddit(client_id=config["client_id"],
+			#client_secret=config["client_secret"],
+			#username=config["username"],
+			#password=config["password"],
+			#user_agent=config["user_agent"])
+
+	reddit = praw.Reddit(client_id=client_id,
+			client_secret=client_secret,
+			username=username,
+			password=password,
+			user_agent=user_agent)
+
+	#trying to get pydash to work
+	#need to get _.map functioning like its js counterpart
+	#reddit = praw.Reddit(client_id=_.map(config,"client_id"), client_secret=_.map(config,"client_secret"), username=_.map(config,"username"),password=_.map(config,"password"),user_agent=_.map(config,"user_agent"))
 
 	#print out our user name
 	print("Authenicated!")
@@ -44,11 +68,22 @@ def getConfigData():
 
 	with open('botCredentials.json') as data_file:
     		data = json.load(data_file)
-	pprint(data)
+	#pprint(data)
+	
 
 	return data
 
+
+def parseConfig():
+	config = ConfigParser.RawConfigParser()
+	config.read("botSettings.cfg")
+
+	#print(config.get("Credentials", "client_id"))
+	return config
+
+config = parseConfig()
+
 #get_comments(reddit)
-config = getConfigData()
-reddit = authenticate(config)
-getComments(reddit)
+#config = getConfigData()
+#reddit = authenticate(config)
+#getComments(reddit)
