1131a0334c5811829fdda57c33ed612e63b7e6e9
Ryan Pinkard <thatpinkardguy@gmail.com>
2017-08-14 01:20:56 +0000

    added getting configuration from json

--- gitGudBot.py
+++ gitGudBot.py
@@ -1,37 +1,53 @@
 
 import praw
+import json
+from pprint import pprint
 
+def authenticate(config):
 
-def authenticate():
-
+	#authenticate with Reddit
 	print("Attempting to authenticate....")
 	reddit = praw.Reddit(client_id='BfFQSyA5YBU6BQ', client_secret='qpNXjEygoorolN5IxUta0Vm9_SE', password='Apple300!', user_agent='testing connection to Reddit', username='thatpinkardguy')
 
-	print("Authenicated as:")
-	print(reddit.user.me())
+	#print out our user name
+	print("Authenicated!")
+	#print(reddit.user.me())
 
+	#return the reddit object
 	return reddit
 
 
-def get_comments(reddit):
+def getComments(reddit):
 
+	#Our subreddit Name
 	subName = 'test'
+
+	#number of comments to pull
 	commentsLimit = 25
 
 	print("Getting "+ str(commentsLimit) +  " comments in subReddit: " + subName)
 
+	#get the subreddit obj
 	subReddit = reddit.subreddit(subName)
 
+	#pull the comments
 	comments = subReddit.comments(limit = commentsLimit)
 
+	#loop through them and print the author's name
 	for comment in comments:
 		print(comment.author)
 
-#	property_names=[p for p in dir(comments) if isinstance(getattr(comments,p),property)]
 
-#	for p in property_names :
-#		print(p.name)
+def getConfigData():
 
+	print('getting configuration data')
 
-reddit = authenticate()
-get_comments(reddit)
+	with open('botCredentials.json') as data_file:
+    		data = json.load(data_file)
+	pprint(data)
+
+	return data
+
+#get_comments(reddit)
+config = getConfigData()
+reddit = authenticate(config)
